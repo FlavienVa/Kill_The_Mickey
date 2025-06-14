3dc8e0e57cf4efddef9ffd82bf6972f21b52ba3e
fannysj <93663750+fannysj@users.noreply.github.com>
2025-06-14 18:45:41 +0100

    printer

--- scripts/printer.gd
+++ scripts/printer.gd
@@ -4,10 +4,30 @@ class_name Printer
 var health := 5
 var is_destroyed := false
 
+var shake_timer := 0.0
+var shake_duration := 0.2  # seconds
+var shake_strength := 7.0
+var original_position: Vector2
+
+func _ready() -> void:
+	original_position = position
+	
+func _process(delta: float) -> void:
+	if shake_timer > 0.0:
+		shake_timer -= delta
+		var offset = Vector2(
+			randf_range(-shake_strength, shake_strength),
+			0
+	)
+		position = original_position + offset
+	else:
+		position = original_position
+
 func take_damage(amount: int, source_position: Vector2) -> void:
 	if is_destroyed:
 		return
-
+		
+	shake()
 	health -= amount
 	print("Printer hit! Remaining HP: ", health)
 
@@ -15,4 +35,8 @@ func take_damage(amount: int, source_position: Vector2) -> void:
 		health = 0
 		is_destroyed = true
 		print("Printer destroyed!")
-		hide()  # or queue_free()
+		$Sprite2D.texture = preload("res://assets/sprites/broken-printer.png")
+
+
+func shake():
+	shake_timer = shake_duration
